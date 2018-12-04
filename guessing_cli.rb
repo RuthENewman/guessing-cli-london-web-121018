c4d326c1a188498a11621e30ea228ccce6e88439
ruthnewman <ruthnewman@gmail.com>
2018-12-04 23:09:47 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+def run_guessing_game
+  
+  while number = ''
+   puts "Please enter a number: "
+  
+  number = gets.chomp
+  if number == 
+    puts "You guessed the correct number!"
+  else 
+    puts 
+end 
